a52e1f2c29243e5c2175945a97762b07ffc88ed7
Marina Kholodova <marinaholodova@gmail.com>
2025-09-02 16:50:43 +0300

    feat: add footer

---
+++ src/constants/footer.ts
@@ -0,0 +1,12 @@
+﻿export const DEVELOPERS = [
+  { nameKey: 'ap', github: 'https://github.com/AliaksaPlh' },
+  { nameKey: 'dy', github: 'https://github.com/ElGansoDorado' },
+  { nameKey: 'mk', github: 'https://github.com/makholodova' },
+];
+
+export const FOOTER_INFO = {
+  rsSchool: 'https://rs.school/courses/reactjs',
+  year: '2025',
+  logoRsSchool: '/rs-school-logo.svg',
+  logoGithub: '/icon-github.svg',
+};
