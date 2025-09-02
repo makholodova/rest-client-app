c505eb0c5028c3b43a4839cdd5412a0c7dc669b7
Marina Kholodova <marinaholodova@gmail.com>
2025-09-02 15:49:03 +0300

    feat: add LanguageSwitcher

---
+++ src/components/ui/language-switcher/language-switcher.tsx
@@ -0,0 +1,35 @@
+﻿'use client';
+
+import { useLocale } from 'next-intl';
+import { Link, usePathname } from '@/i18n/navigation';
+import Image from 'next/image';
+import styles from './language-switcher.module.scss';
+import { locales } from '@/constants/languages';
+
+export default function LanguageSwitcher() {
+  const active = useLocale();
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.wrapper}>
+      {locales.map(({ code, label, flag }) => (
+        <Link
+          key={code}
+          href={pathname}
+          locale={code}
+          prefetch={false}
+          className={`${styles.link} ${active === code ? styles.active : styles.inactive}`}
+        >
+          <Image
+            className={styles.image}
+            priority={false}
+            src={flag}
+            alt={label}
+            width={48}
+            height={36}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
---
+++ src/constants/languages.ts
@@ -0,0 +1,4 @@
+﻿export const locales = [
+  { code: 'en', label: 'English', flag: '/en.svg' },
+  { code: 'ru', label: 'Русский', flag: '/ru.svg' },
+];
