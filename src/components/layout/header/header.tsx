3bc8945928f17ef690fb643b1af219b8524265b4
Marina Kholodova <marinaholodova@gmail.com>
2025-09-03 10:32:52 +0300

    feat: add sticky header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,15 +1,19 @@
-﻿import styles from './header.module.scss';
+﻿'use client';
+
+import styles from './header.module.scss';
 import Logo from '@/components/ui/logo/logo';
 import Link from 'next/link';
 import { ROUTES } from '@/constants/routes';
 import { useTranslations } from 'next-intl';
 import LanguageSwitcher from '@/components/ui/language-switcher/language-switcher';
+import { useIsScrolled } from '@/hooks/useIsScrolled';
 
 export default function Header() {
+  const isScrolled = useIsScrolled();
   const t = useTranslations('Header');
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isScrolled && styles.sticky}`}>
       <div className={`${styles.headerWrapper} container`}>
         <Logo />
         <div className={styles.nav}>
