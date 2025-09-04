9c41d3e2267e86e4a24650d03005dabdf670c2c5
Aliaksa <sashvero@gmail.com>
2025-09-04 06:05:07 +0300

    feat: add signout btn, place SIN, SOUT, SUP buttons as they should be

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -7,10 +7,13 @@ import { ROUTES } from '@/constants/routes';
 import { useTranslations } from 'next-intl';
 import LanguageSwitcher from '@/components/ui/language-switcher/language-switcher';
 import { useIsScrolled } from '@/hooks/useIsScrolled';
+import { logout, auth } from '../../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Header() {
   const isScrolled = useIsScrolled();
   const t = useTranslations('Header');
+  const [user] = useAuthState(auth);
 
   return (
     <header className={`${styles.header} ${isScrolled && styles.sticky}`}>
@@ -18,18 +21,26 @@ export default function Header() {
         <Logo />
         <div className={styles.nav}>
           <LanguageSwitcher />
-          <Link
-            className={`${styles.navLink} ${styles.signIn}`}
-            href={ROUTES.SIGN_IN}
-          >
-            {t('sign-in')}
-          </Link>
-          <Link
-            className={`${styles.navLink} ${styles.signUp}`}
-            href={ROUTES.SIGN_UP}
-          >
-            {t('sign-up')}
-          </Link>
+          {!user ? (
+            <>
+              <Link
+                className={`${styles.navLink} ${styles.signIn}`}
+                href={ROUTES.SIGN_IN}
+              >
+                {t('sign-in')}
+              </Link>
+              <Link
+                className={`${styles.navLink} ${styles.signUp}`}
+                href={ROUTES.SIGN_UP}
+              >
+                {t('sign-up')}
+              </Link>
+            </>
+          ) : (
+            <button className={styles.signOut} onClick={logout}>
+              Sign Out
+            </button>
+          )}
         </div>
       </div>
     </header>
