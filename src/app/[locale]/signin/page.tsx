047550015b73adb22693780330f958ac603798e4
Aliaksa <sashvero@gmail.com>
2025-09-04 04:05:45 +0300

    feat: add registration and signin pages

--- src/app/[locale]/signin/page.tsx
+++ src/app/[locale]/signin/page.tsx
@@ -1,3 +1,56 @@
-﻿export default function SignInPage() {
-  return <div>Логин</div>;
+﻿'use client';
+import { useState, useEffect } from 'react';
+import { auth, logInWithEmailAndPassword } from '../../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '../../../constants/routes';
+import Link from 'next/link';
+
+export default function SignInPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) router.push(ROUTES.HOME);
+    if (error)
+      alert('Please, try later or sign in in case you are a new user.');
+  }, [user, loading, router, error]);
+
+  const handleSignIn = async () => {
+    await logInWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <div className="login">
+      <div className="login_container">
+        <input
+          type="text"
+          className="login_textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login_textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button className="login_btn" onClick={handleSignIn}>
+          Sign In
+        </button>
+      </div>
+      <div>
+        <p className="login_signup-text">
+          <Link href={ROUTES.SIGN_UP} className="login_link">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/app/[locale]/signup/page.tsx
+++ src/app/[locale]/signup/page.tsx
@@ -1,3 +1,63 @@
-﻿export default function SignUpPage() {
-  return <div>Регистрация</div>;
+﻿'use client';
+import { useState, useEffect } from 'react';
+import { auth, registerWithEmailAndPassword } from '../../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '../../../constants/routes';
+import Link from 'next/link';
+
+export default function SignUpPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  const register = () => {
+    if (!name) alert('Please enter name');
+    registerWithEmailAndPassword(name, email, password);
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) router.push(ROUTES.HOME);
+  }, [user, loading, router]);
+
+  return (
+    <div className="register">
+      <div className="register_container">
+        <input
+          type="text"
+          className="register_textBox"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full Name"
+        />{' '}
+        <input
+          type="text"
+          className="register_textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="register_textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button className="register_btn" onClick={register}>
+          Sign Up
+        </button>
+      </div>
+      <div>
+        <p className="register__signup-text">
+          <Link href={ROUTES.SIGN_IN} className="register_link">
+            Sign In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
