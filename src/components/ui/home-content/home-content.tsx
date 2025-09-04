0d91795bf8e5dc080b25219ffcf702b2ff99a84f
Marina Kholodova <marinaholodova@gmail.com>
2025-09-04 14:52:52 +0300

    feat: add team section with members data and cards

---
+++ src/components/ui/home-content/home-content.tsx
@@ -0,0 +1,10 @@
+﻿import styles from './home-content.module.scss';
+import TeamMembers from '@/components/ui/team-members/team-members';
+
+export default function HomeContent() {
+  return (
+    <section className={styles.wrapper}>
+      <TeamMembers />
+    </section>
+  );
+}
