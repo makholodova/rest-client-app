a720a89ce7c6bfe7cc6ba03bdf836812863b6956
Marina Kholodova <marinaholodova@gmail.com>
2025-09-04 16:09:40 +0300

    feat: add course and project info

--- src/components/ui/home-content/home-content.tsx
+++ src/components/ui/home-content/home-content.tsx
@@ -1,10 +1,26 @@
 ﻿import styles from './home-content.module.scss';
 import TeamMembers from '@/components/ui/team-members/team-members';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 
 export default function HomeContent() {
+  const t = useTranslations('HomePage');
   return (
     <section className={styles.wrapper}>
+      <div className={styles.project}>
+        <h2 className={styles.title}>{t('projectTitle')}</h2>
+        <p className={styles.description}>{t('projectText')}</p>
+      </div>
+
       <TeamMembers />
+
+      <div className={styles.course}>
+        <Image src="/pana.png" alt=" RS School" width={307} height={302} />
+        <div className={styles.courseContent}>
+          <h2 className={styles.title}>{t('courseTitle')}</h2>
+          <p className={styles.description}>{t('courseText')}</p>
+        </div>
+      </div>
     </section>
   );
 }
