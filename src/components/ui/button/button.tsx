f0b5e2c252bb2fe512d7d68f7dbc7327c85bf760
Marina Kholodova <marinaholodova@gmail.com>
2025-09-07 15:30:28 +0300

    feat: add base components for button and link

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,31 @@
+﻿import clsx from 'clsx';
+import styles from './button.module.scss';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  variant?: 'primary' | 'secondary';
+  type?: 'button' | 'submit';
+  onClick?: () => void;
+  disabled?: boolean;
+};
+
+export default function Button({
+  children,
+  className,
+  variant = 'primary',
+  type = 'button',
+  onClick,
+  disabled,
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={clsx(styles.button, styles[variant], className)}
+    >
+      {children}
+    </button>
+  );
+}
