45b9c5e396947fa2440a1f7767613065c2beb5a9
Aliaksa <sashvero@gmail.com>
2025-09-08 00:52:00 +0300

    Fix: rewrite sign out button

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -7,10 +7,14 @@ import { useTranslations } from 'next-intl';
 import LanguageSwitcher from '@/components/ui/language-switcher/language-switcher';
 import { useIsScrolled } from '@/hooks/useIsScrolled';
 import ButtonLink from '@/components/ui/button-link/button-link';
+import Button from '@/components/ui/button/button';
+import { logout, auth } from '../../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function Header() {
   const isScrolled = useIsScrolled();
   const t = useTranslations('Header');
+  const [user] = useAuthState(auth);
 
   return (
     <header className={`${styles.header} ${isScrolled && styles.sticky}`}>
@@ -18,16 +22,25 @@ export default function Header() {
         <Logo />
         <div className={styles.nav}>
           <LanguageSwitcher />
-          <ButtonLink
-            className={styles.navLink}
-            variant={'secondary'}
-            href={ROUTES.SIGN_IN}
-          >
-            {t('sign-in')}
-          </ButtonLink>
-          <ButtonLink className={styles.navLink} href={ROUTES.SIGN_UP}>
-            {t('sign-up')}
-          </ButtonLink>
+          {!user ? (
+            <ButtonLink
+              className={styles.navLink}
+              variant={'secondary'}
+              href={ROUTES.SIGN_IN}
+            >
+              {t('sign-in')}
+            </ButtonLink>
+          ) : null}
+          {!user ? (
+            <ButtonLink className={styles.navLink} href={ROUTES.SIGN_UP}>
+              {t('sign-up')}
+            </ButtonLink>
+          ) : null}
+          {user ? (
+            <Button className={styles.navLink} onClick={logout}>
+              {t('sign-out')}
+            </Button>
+          ) : null}
         </div>
       </div>
     </header>
