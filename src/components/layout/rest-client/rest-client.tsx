0e1ca558b2a589a563581c4fb0753bfd9058eb5e
Marina Kholodova <marinaholodova@gmail.com>
2025-09-09 14:28:45 +0300

    feat: add RestClient

--- src/components/layout/rest-client/rest-client.tsx
+++ src/components/layout/rest-client/rest-client.tsx
@@ -7,38 +7,55 @@ import { METHOD_OPTIONS } from '@/constants/rest-client';
 import { FieldInput } from '@/components/ui/field-input/field-input';
 import { useTranslations } from 'next-intl';
 import Button from '@/components/ui/button/button';
+import { Tabs } from '@/components/ui/tabs/tabs';
+
+import Headers from '@/components/layout/rest-client/tabs/headers/headers';
+import ResponseViewer from '@/components/layout/rest-client/response-viewer/response-viewer';
+import BodyEditor from '@/components/layout/rest-client/tabs/body-editor/body-editor';
+import CodeGenerator from '@/components/layout/rest-client/tabs/code-generator/code-generator';
 
 export default function RestClient() {
+  const t = useTranslations('RestClient');
+
+  const tabs = [
+    { label: t('headers'), content: <Headers /> },
+    { label: t('body'), content: <BodyEditor /> },
+    { label: t('code'), content: <CodeGenerator /> },
+  ];
+
   const [method, setMethod] = useState<Method>('GET');
   const [url, setUrl] = useState('');
-  const t = useTranslations('RestClient');
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // TODO: отправка запроса
+  };
 
   return (
     <div className={styles.wrapper}>
       <h2>RestClient</h2>
-      <div>
-        <form>
-          <select
-            className={styles.method}
-            value={method}
-            onChange={(e) => setMethod(e.target.value as Method)}
-          >
-            {METHOD_OPTIONS.map((method) => (
-              <option key={method} value={method}>
-                {method}
-              </option>
-            ))}
-          </select>
+      <form className={styles.content} onSubmit={onSubmit}>
+        <select
+          className={styles.method}
+          value={method}
+          onChange={(e) => setMethod(e.target.value as Method)}
+        >
+          {METHOD_OPTIONS.map((method) => (
+            <option key={method} value={method}>
+              {method}
+            </option>
+          ))}
+        </select>
 
-          <FieldInput
-            placeholder="https://api.example.com/resource"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          />
-
-          <Button>{t('send')}</Button>
-        </form>
-      </div>
+        <FieldInput
+          placeholder="https://api.example.com/resource"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <Button type={'submit'}>{t('send')}</Button>
+      </form>
+      <Tabs tabs={tabs} />
+      <ResponseViewer />
     </div>
   );
 }
