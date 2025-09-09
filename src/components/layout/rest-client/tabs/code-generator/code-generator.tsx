79562ee72f970f6f3eea8464fd50414aa833dc31
Marina Kholodova <marinaholodova@gmail.com>
2025-09-09 14:27:02 +0300

    feat: add tabs for Body,Code and Headers

---
+++ src/components/layout/rest-client/tabs/code-generator/code-generator.tsx
@@ -0,0 +1,25 @@
+/*import { useTranslations } from 'next-intl';*/
+import styles from './code-generator.module.scss';
+import { ProgrammingLanguage } from '@/types/postman.type';
+import { LANGUAGE_OPTIONS } from '@/constants/rest-client';
+import { useState } from 'react';
+
+export default function CodeGenerator() {
+  /*const t = useTranslations('RestClient');*/
+  const [language, setLanguage] = useState<ProgrammingLanguage>('javascript');
+  return (
+    <div className={styles.wrapper}>
+      <select
+        className={styles.language}
+        value={language}
+        onChange={(e) => setLanguage(e.target.value as ProgrammingLanguage)}
+      >
+        {LANGUAGE_OPTIONS.map((language) => (
+          <option key={language.key} value={language.key}>
+            {language.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
