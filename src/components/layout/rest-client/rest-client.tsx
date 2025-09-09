fcf8b7fbdfe20cdaef6aa519aac43faa9b74f7af
Marina Kholodova <marinaholodova@gmail.com>
2025-09-09 12:21:46 +0300

    feat: add url and selector methods

--- src/components/layout/rest-client/rest-client.tsx
+++ src/components/layout/rest-client/rest-client.tsx
@@ -1,5 +1,44 @@
+'use client';
+
 import styles from './rest-client.module.scss';
+import { useState } from 'react';
+import { Method } from '@/types/postman.type';
+import { METHOD_OPTIONS } from '@/constants/rest-client';
+import { FieldInput } from '@/components/ui/field-input/field-input';
+import { useTranslations } from 'next-intl';
+import Button from '@/components/ui/button/button';
 
 export default function RestClient() {
-  return <div className={styles.wrapper}>RestClient</div>;
+  const [method, setMethod] = useState<Method>('GET');
+  const [url, setUrl] = useState('');
+  const t = useTranslations('RestClient');
+
+  return (
+    <div className={styles.wrapper}>
+      <h2>RestClient</h2>
+      <div>
+        <form>
+          <select
+            className={styles.method}
+            value={method}
+            onChange={(e) => setMethod(e.target.value as Method)}
+          >
+            {METHOD_OPTIONS.map((method) => (
+              <option key={method} value={method}>
+                {method}
+              </option>
+            ))}
+          </select>
+
+          <FieldInput
+            placeholder="https://api.example.com/resource"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+
+          <Button>{t('send')}</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
