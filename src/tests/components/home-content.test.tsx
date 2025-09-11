99efe5d612796bbca2ad0be1b76a82e2cc2ec17d
Marina Kholodova <marinaholodova@gmail.com>
2025-09-11 21:27:23 +0300

    feat: add test for HomeContent

---
+++ src/tests/components/home-content.test.tsx
@@ -0,0 +1,28 @@
+﻿import { render, screen } from '@testing-library/react';
+import HomeContent from '@/components/ui/home-content/home-content';
+
+jest.mock('next-intl', () => ({
+  useTranslations: () => (key: string) => key,
+}));
+
+jest.mock('next/image', () => (props: any) => {
+  return <img {...props} alt={props.alt} />;
+});
+
+jest.mock('@/components/ui/team-members/team-members', () => () => (
+  <div data-testid="team-members" />
+));
+
+describe('HomeContent', () => {
+  it('рендерит projectText, courseTitle, courseText и TeamMembers', () => {
+    render(<HomeContent />);
+
+    expect(screen.getByText('projectText')).toBeInTheDocument();
+    expect(screen.getByText('courseTitle')).toBeInTheDocument();
+    expect(screen.getByText('courseText')).toBeInTheDocument();
+
+    expect(screen.getByTestId('team-members')).toBeInTheDocument();
+
+    expect(screen.getByAltText('RS School')).toBeInTheDocument();
+  });
+});
