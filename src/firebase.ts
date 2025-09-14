cfec5aa9ea68aced3b96a4d83d6b67af69da87c8
Aliaksa <sashvero@gmail.com>
2025-09-14 22:10:07 +0300

    refactor: remove firebaseConfig keys to env.local

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,13 +12,13 @@ import { toast } from 'react-toastify';
 import { FirebaseError } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBO_yryYqJQVYRsV4gD0NZ8Ulplxw96Kz8',
-  authDomain: 'rest-client-app-cfaa5.firebaseapp.com',
-  projectId: 'rest-client-app-cfaa5',
-  storageBucket: 'rest-client-app-cfaa5.firebasestorage.app',
-  messagingSenderId: '807816489019',
-  appId: '1:807816489019:web:253791a85b40cc0eefd41e',
-  measurementId: 'G-ZVR99WFE0W',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
