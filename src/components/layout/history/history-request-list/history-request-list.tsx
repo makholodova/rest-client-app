04f0fc2a30da405ca64f7f9f1ca51d4f1359e080
Marina Kholodova <marinaholodova@gmail.com>
2025-09-14 22:46:20 +0300

    feat: add HistoryRequestsList

---
+++ src/components/layout/history/history-request-list/history-request-list.tsx
@@ -0,0 +1,25 @@
+﻿import styles from './history-request-list.module.scss';
+import HistoryRequestItem from '@/components/layout/history/history-request-item/history-request-item';
+import EmptyHistory from '@/components/layout/history/empty-history/empty-history';
+import { HistoryRequest } from '@/types/history.type';
+
+type HistoryRequestsListProps = {
+  requests: HistoryRequest[];
+};
+export default function HistoryRequestsList({
+  requests,
+}: HistoryRequestsListProps) {
+  return (
+    <div>
+      {!requests?.length ? (
+        <EmptyHistory />
+      ) : (
+        <ul className={styles.list}>
+          {requests.map((request) => (
+            <HistoryRequestItem key={request.id} request={request} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
